c480f777e018866961e4787a35a92d7344e5b4a7
EdwardKGN <edward.96@live.com>
2026-04-09 22:18:53 +0800

    Update the Pie Chart to use the latest format

--- frontend/src/components/ExpensePieChart.jsx
+++ frontend/src/components/ExpensePieChart.jsx
@@ -1,4 +1,40 @@
-import { PieChart, Pie, Cell, Tooltip, Legend} from "recharts";
+import { PieChart, Pie, Sector, Tooltip, Legend } from "recharts";
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#A28AFF"];
+
+function ExpensePieChart( {data} ) {
+    const coloredData = data.map((entry, index) => ({
+        ...entry,
+        fill: COLORS[index % COLORS.length]
+    })); // Manually assign color fill for each data for Legend to refer to
+
+    return (
+        <PieChart width={400} height={400}>
+            <Pie
+                data={coloredData}
+                dataKey="total"
+                nameKey="category"
+                cx="50%"
+                cy="50%"
+                outerRadius={120}
+                label
+                shape={(props) => {
+                    const color = COLORS[props.index % COLORS.length];
+                    return <Sector {...props} fill={color} />;
+                }}
+            >
+            </Pie>
+
+            <Tooltip />
+            <Legend />
+        </PieChart>
+    )
+}
+
+export default ExpensePieChart
+
+/*
+Legacy Codes
 
 function ExpensePieChart( {data} ) {
     return (
@@ -23,4 +59,4 @@ function ExpensePieChart( {data} ) {
     )
 }
 
-export default ExpensePieChart
+*/
